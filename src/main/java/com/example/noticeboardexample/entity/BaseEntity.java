73e368cc52558d59d2b930922194787b8962a1b1
seokho <seokho7799@gmail.com>
2025-03-09 20:47:12 +0900

    fix : BaseEntity 필드 타입 수정
    - String -> LocalDateTime

--- src/main/java/com/example/noticeboardexample/entity/BaseEntity.java
+++ src/main/java/com/example/noticeboardexample/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.example.noticeboardexample.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,9 +13,9 @@ public class BaseEntity {
 
   @CreatedDate
   @Column(name = "created_at", updatable = false)
-  private String createdAt;
+  private LocalDateTime createdAt;
 
   @LastModifiedDate
   @Column(name = "updated_at")
-  private String updatedAt;
+  private LocalDateTime updatedAt;
 }
