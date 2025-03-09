0ac4cd583ac4292585c700c92ae2acaef89148a8
seokho <seokho7799@gmail.com>
2025-03-09 19:16:16 +0900

    init : 프로젝트 초기 설정

---
+++ src/main/java/com/example/noticeboardexample/NoticeboardexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeboardexampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoticeboardexampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/noticeboardexample/NoticeboardexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noticeboardexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeboardexampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
