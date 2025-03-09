630e97aa7dcb11fbbbd450518937c427aadf14dd
seokho <seokho7799@gmail.com>
2025-03-09 19:36:33 +0900

    feat : 게시글 서비스 생성
    - 게시글 전체 목록 조회
    - 게시글 단일 조회
    - 게시글 저장
    - 게시글 업데이트
    - 게시글 삭제

---
+++ src/main/java/com/example/noticeboardexample/service/PostService.java
@@ -0,0 +1,65 @@
+package com.example.noticeboardexample.service;
+
+import com.example.noticeboardexample.entity.Post;
+import com.example.noticeboardexample.repository.PostRepository;
+import com.example.noticeboardexample.service.dto.PostCreateRequest;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+  private final PostRepository postRepository;
+
+  public List<Post> findAll() {
+    return postRepository.findAll();
+  }
+
+  public Post findById(Long id) {
+    return postRepository.findById(id).orElse(null);
+  }
+
+  @Transactional
+  public Post save(PostCreateRequest postCreateRequest) {
+    Post post = Post.builder()
+        .writerName(postCreateRequest.writerName())
+        .password(postCreateRequest.password())
+        .title(postCreateRequest.title())
+        .content(postCreateRequest.content())
+        .build();
+
+    return postRepository.save(post);
+  }
+
+  @Transactional
+  public Long deleteById(Long id, String password) {
+    Post post = postRepository.findById(id)
+            .orElseThrow(NoSuchElementException::new);
+
+    validatePassword(password, post.getPassword());
+
+    postRepository.deleteById(id);
+    return id;
+  }
+
+  private void validatePassword(String inputPassword, String password) {
+    if (inputPassword == null || !inputPassword.equals(password)) {
+      throw new SecurityException("비밀번호가 일치하지 않습니다.");
+    }
+  }
+
+  @Transactional
+  public Post updateContent(Long id, String password, String content) {
+    Post post = postRepository.findById(id)
+        .orElseThrow(NoSuchElementException::new);
+
+    validatePassword(password, post.getPassword());
+
+    post.updateContent(content);
+    return post;
+  }
+}
