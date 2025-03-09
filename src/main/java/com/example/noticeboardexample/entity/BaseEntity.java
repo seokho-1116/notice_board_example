ede45a44365916fa7e93ee958926e70ab3799a3e
seokho <seokho7799@gmail.com>
2025-03-09 20:37:06 +0900

    fix : auditing 애너테이션 추가

--- src/main/java/com/example/noticeboardexample/entity/BaseEntity.java
+++ src/main/java/com/example/noticeboardexample/entity/BaseEntity.java
@@ -2,13 +2,17 @@ package com.example.noticeboardexample.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @MappedSuperclass
 public class BaseEntity {
 
+  @CreatedDate
   @Column(name = "created_at", updatable = false)
   private String createdAt;
 
+  @LastModifiedDate
   @Column(name = "updated_at")
   private String updatedAt;
 }
