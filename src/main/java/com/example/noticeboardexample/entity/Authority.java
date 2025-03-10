67655114ab1f02fbb3d7b24ff124e52257f9f1c1
seokho <seokho7799@gmail.com>
2025-03-10 20:40:13 +0900

    feat : 사용자 엔티티 추가

---
+++ src/main/java/com/example/noticeboardexample/entity/Authority.java
@@ -0,0 +1,5 @@
+package com.example.noticeboardexample.entity;
+
+public enum Authority {
+  ADMIN, USER
+}
---
+++ src/main/java/com/example/noticeboardexample/entity/EndUser.java
@@ -0,0 +1,32 @@
+package com.example.noticeboardexample.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "end_user")
+public class EndUser extends BaseEntity {
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "user_password")
+  private String userPassword;
+
+  @Column(name = "authority")
+  @Enumerated(EnumType.STRING)
+  private Authority authority;
+}
