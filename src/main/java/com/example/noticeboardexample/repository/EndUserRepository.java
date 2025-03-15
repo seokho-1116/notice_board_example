cc3aadd175040a9d43889a0fef1d261e2f125497
seokho <seokho7799@gmail.com>
2025-03-15 22:03:52 +0900

    refactor : JpaRepository -> Repository로 변경

--- src/main/java/com/example/noticeboardexample/repository/EndUserRepository.java
+++ src/main/java/com/example/noticeboardexample/repository/EndUserRepository.java
@@ -2,9 +2,11 @@ package com.example.noticeboardexample.repository;
 
 import com.example.noticeboardexample.entity.EndUser;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
-public interface EndUserRepository extends JpaRepository<EndUser, Long> {
+public interface EndUserRepository extends Repository<EndUser, Long> {
 
   Optional<EndUser> findByUsername(String username);
+
+  EndUser save(EndUser endUser);
 }
