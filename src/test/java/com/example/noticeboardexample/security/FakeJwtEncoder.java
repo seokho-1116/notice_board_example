3af3a64e4d029b5e70e591ff22fa029a6496bf9e
seokho <seokho7799@gmail.com>
2025-03-18 22:48:17 +0900

    test : RSA key pair 생성 위치 이동

--- src/test/java/com/example/noticeboardexample/security/FakeJwtEncoder.java
+++ src/test/java/com/example/noticeboardexample/security/FakeJwtEncoder.java
@@ -1,5 +1,6 @@
 package com.example.noticeboardexample.security;
 
+import com.example.noticeboardexample.util.TestDataUtil;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -7,8 +8,6 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -22,21 +21,11 @@ public class FakeJwtEncoder implements JwtEncoder {
   private final NimbusJwtEncoder nimbusJwtEncoder;
 
   public FakeJwtEncoder() {
-    KeyPair rsaKeys = generateRsaKey();
+    KeyPair rsaKeys = TestDataUtil.generateKeyPair();
     JWK jwk = new RSAKey.Builder((RSAPublicKey) rsaKeys.getPublic()).privateKey((RSAPrivateKey) rsaKeys.getPrivate()).build();
     JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
     this.nimbusJwtEncoder = new NimbusJwtEncoder(jwks);
   }
-
-  private KeyPair generateRsaKey() {
-    try {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(2048);
-        return keyPairGenerator.generateKeyPair();
-    } catch (NoSuchAlgorithmException e) {
-        throw new IllegalStateException(e);
-    }
-}
 
   @Override
   public Jwt encode(JwtEncoderParameters parameters) throws JwtEncodingException {
--- src/test/java/com/example/noticeboardexample/util/TestDataUtil.java
+++ src/test/java/com/example/noticeboardexample/util/TestDataUtil.java
@@ -1,11 +1,24 @@
 package com.example.noticeboardexample.util;
 
 import java.lang.reflect.Field;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 
 public class TestDataUtil {
 
   private TestDataUtil() {
 
+  }
+
+  public static KeyPair generateKeyPair() {
+    try {
+      KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+      keyPairGenerator.initialize(2048);
+      return keyPairGenerator.generateKeyPair();
+    } catch (NoSuchAlgorithmException e) {
+      throw new IllegalStateException(e);
+    }
   }
 
   public static <T> void forceSetId(T entity, long id) {
